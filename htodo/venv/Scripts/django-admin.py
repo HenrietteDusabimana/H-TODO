5bd35958139a93836578e35a07fe6d1e4c63e10d
Henriette Dusabimana <hdusabim@gmail.com>
2021-08-03 21:30:18 +0200

    added the requirements file and installed it

---
+++ htodo/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\hp\documents\learning\django-rest-framework\h-todo\htodo\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
